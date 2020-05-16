62bfa0ef09cb081eb71b8593f79cafa5fab138ed
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-16 15:35:11 +0530

    nodemon not working properly

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const path = require('path');
 
 const app = express();
 
+//app.get('/', (req,res)=>{
+//    res.send(`<h1>Hello World?</h1>`);
+//});
+
 //Static Folder
 app.use(express.listen(path.join(__dirname,'public')));
 
