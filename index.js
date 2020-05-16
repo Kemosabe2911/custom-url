d061a206c0afdbdec147a07c13ae4ad03b9ca0ef
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-16 17:03:55 +0530

    Stuck somewhere

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req,res) =>{
+    console.log('Calling app.get');
+    res.send('<h1>Hello World</h1>');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`Server is running at PORT ${PORT}`));
