ada974847700529640a8c378e2d0566d395cbdf3
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-16 15:15:06 +0530

    Add static folder

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+//Static Folder
+app.use(express.listen(path.join(__dirname,'public')));
 
 const PORT = process.env.PORT || 5000;
 
