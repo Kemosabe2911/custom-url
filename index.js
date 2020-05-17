046ab1988ae9ee955c8bcdf85dff0d8dca3d8ca4
Kemosabe2911 <stevinprince29@gmail.com>
2020-05-17 16:04:49 +0530

    install path

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const axios = require("axios");
 const path= require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname,'public','index.html')));
+//Static Folder
+//  app.use(express.static(path.join(__dirname,'public','index.html')));
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname,'public','index.html'));
+});
 
    /* app.get('/', ( req , res) =>{
         console.log('Calling app.get');
